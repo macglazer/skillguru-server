7cd2c85efdbcc8af10c9551e21155ceee7de4aa4
maciejmaciek <hello@maciejglazer.com>
2023-05-28 19:33:43 +0200

    first init

---
+++ src/main/java/com/server/skillguru/SkillguruServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.skillguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillguruServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillguruServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/skillguru/SkillguruServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.skillguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillguruServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
